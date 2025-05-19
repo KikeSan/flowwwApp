1686b2938582e42213d46725cabfef2795825e84
enrique.sanchez <kikin1601@hotmail.com>
2025-05-19 18:36:09 +0200

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   pathGH: 'https://api.github.com/search/users?q=',
   all: 'https://api.github.com/users/',
   tkGH: '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',
-  version: '1.0.2'
+  version: '1.0.3'
 };
 
 /*
